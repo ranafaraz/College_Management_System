2b7774c4f270f897c6d79b314d176f66b78c7d3f
nauman <someone@nowhere.com>
2017-07-22 14:58:29 +0500

    Created new file page.php

--- page.php
+++ page.php
@@ -14,56 +14,7 @@
 
     <!-- Main content -->
     <section class="content container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="box box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">Carousel</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
-                </ol>
-                <div class="carousel-inner">
-                  <div class="item active">
-                    <img src="dist/img/img.jpg" alt="First slide" width="100%" height="300px">
-
-                    <div class="carousel-caption">
-                      First Slide
-                    </div>
-                  </div>
-                  <div class="item">
-                    <img src="dist/img/img.jpg" alt="Second slide"width="100%" height="300px">
-
-                    <div class="carousel-caption">
-                      Second Slide
-                    </div>
-                  </div>
-                  <div class="item">
-                    <img src="dist/img/img.jpg" alt="Third slide"width="100%" height="300px">
-
-                    <div class="carousel-caption">
-                      Third Slide
-                    </div>
-                  </div>
-                </div>
-                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                  <span class="fa fa-angle-left"></span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                  <span class="fa fa-angle-right"></span>
-                </a>
-              </div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-      </div>
+     
     </section>
     <!-- /.content -->
   </div>
